3c81b444253db70715c345cd1d639f783a2a43ce
Yves Yuen <yves.j.yuen@gmail.com>
2015-09-29 03:27:06 -0700

    added blue styling to css

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,5 +1,10 @@
 app.controller('HomeController', function($scope, Fixtures, Position, Variable) {
 
+    $scope.welcomeMsg = `Welcome to Crunch.io!
+    Please select a survey question from the catalog on your left.`;
+
+
+
     console.log(Position.get("Recommendation")); //should be 0
     console.log(Position.get("Aided Awareness (Logo)")); //should be last
 
